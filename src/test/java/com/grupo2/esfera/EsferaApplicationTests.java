7383249843107b8502bc9fbf83bbaf6f0bc03494
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2024-03-07 20:38:26 -0300

    first commit

---
+++ src/test/java/com/grupo2/esfera/EsferaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo2.esfera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsferaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
