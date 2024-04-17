d71acd943f37efb3206c54c5e50e995020e736e0
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2024-04-16 22:09:20 -0300

    #5 formulario cadastro cliente (#5)
    * Update client.html
    
    * abrir e fechar popup
    
    * fetch post front, ClientAddressContactDTO, service add dto

---
+++ back-end/src/main/java/com/esfera/g2/esferag2/model/ClientAddressContact.java
@@ -0,0 +1,31 @@
+package com.esfera.g2.esferag2.model;
+
+public class ClientAddressContact {
+    Client client;
+    Address address;
+    Contact contact;
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+}
