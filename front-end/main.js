b4ab3ae704a4b5c062de2adfbc1b621c94eebf58
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2024-06-22 15:24:52 -0300

    Remoção de diretórios não utilizados e populador

--- front-end/main.js
+++
@@ -1,7 +0,0 @@
-function toggleDropdown() {
-    document.querySelector('.dropdown').classList.toggle('hidden');
-}
-
-const toggleSidebar = () => {
-    document.querySelector('.sidebar').classList.toggle('active');
-}
